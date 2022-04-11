93c5d2f6d216c93208ddce03a51ab79b643b5080
憨憨憨憨憨八嘎 <1124533793@qq.com>
2022-04-11 12:16:31 +0800

    初始模板构建part1

---
+++ Assets/Scripts/UIFramework/BasePanel/BasePanel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//所有UI面板的父类，包含UI面板的状态信息
+public abstract class BasePanel
+{
+    public UIType UIType { get; private set; }
+    //UI进入的时候执行的操作
+    public virtual void OnEnter() { }
+    //UI暂停时执行的操作
+    public virtual void OnPause()
+    {
+
+    }
+    //UI继续的时候执行的操作
+    public virtual void OnResume()
+    {
+
+    }
+    //UI退出的时候执行的操作
+    public virtual void OnExit()
+    {
+
+    }
+
+    
+}
